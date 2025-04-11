2384a0653a2e75b97f74a4553f59e5cd3379eef5
David Preiss <dnpreiss@gmail.com>
2025-04-11 12:45:22 -0400

    split up big string

--- GeneratePDFs.py
+++ GeneratePDFs.py
@@ -4,7 +4,10 @@ pdf_path = './pdf_folder'
 
 pdf_files = [f for f in os.listdir(pdf_path) if f.endswith('.pdf')]
 
-html_content = "<!DOCTYPE html>\n<html>\n<head>\n<title>PDF Files</title>\n</head>\n<body>\n<h2>All PDFs</h2>\n<ul>\n"
+html_content = {
+    "<!DOCTYPE html>\n<html>\n<head>\n<title>PDF Files</title>\n</head>\n<body>\n" +
+    "<h2>All PDFs</h2>\n<ul>\n"
+    }
 for pdf in pdf_files:
     html_content += f'    <li><a target="_blank" href="{pdf_path}/{pdf}" download>{pdf}</a></li>\n'
 html_content += "</ul>\n</body>\n</html>"
