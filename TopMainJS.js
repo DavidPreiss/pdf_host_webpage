674bba039b1855342101b5510796eeeb89e45041
David Preiss <dnpreiss@gmail.com>
2025-04-15 12:25:19 -0400

    changed target folder to Customers

--- TopMainJS.js
+++ TopMainJS.js
@@ -4,7 +4,8 @@ let lastname = "lastname";
 function dispName()
 {
     firstname = document.getElementById("userInput").value;
-	const newURL = "./Users/User"+firstname+"/User"+firstname+".html";
+	//const newURL = "./Users/User"+firstname+"/User"+firstname+".html";
+	const newURL = "./Customers/C"+firstname+"/C"+firstname+".html";
     document.getElementById("nameshower").innerHTML=newURL;
 	
 	const mylink = document.getElementById("namelink");
